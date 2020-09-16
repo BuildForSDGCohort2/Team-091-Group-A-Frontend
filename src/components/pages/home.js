0440031fc6e04fa017f8ff933dbb7e4ebf4a1d85
its-nedum <de.magnificent1@gmail.com>
2020-09-16 16:02:15 +0100

    feature(home search form in progress)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,14 +1,29 @@
 import React from "react"
 import "../../styles/home.css"
-// import Navbar from "../layouts/navbar"
 
 const Home = () => {
     return (
         <div>
             <div className="container-fluid home-banner">
-                {/* <Navbar /> */}
                 <div className="row">
-
+                    <div className="col-md-2"></div>
+                    <div className="col-12 col-sm-8 card bg-transparent mt-5">
+                        <div className="row mt-4 mb-4">
+                            <div className="col-12 col-sm-4 input-group pr-0">
+                                <input type="search" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-3 input-group pr-0">
+                                <input type="search" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-3 input-group pr-0">
+                                <input type="search" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-2 pr-0">
+                                <button className="btn btn-primary">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2"></div>
                 </div>
             </div>
         </div>
