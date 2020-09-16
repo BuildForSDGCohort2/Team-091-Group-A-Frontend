db9cfa8ff8bdaa4dba5283c56ddd6bf91c522e71
its-nedum <de.magnificent1@gmail.com>
2020-09-16 14:52:52 +0100

    chores(switch the design)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Home from "./components/pages/home";
-// import Navbar from "./components/layouts/navbar";
+import Navbar from "./components/layouts/navbar";
 import Footer from "./components/layouts/footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "../../styles/home.css"
-import Navbar from "../layouts/navbar"
+// import Navbar from "../layouts/navbar"
 
 const Home = () => {
     return (
         <div>
             <div className="container-fluid home-banner">
-                <Navbar />
+                {/* <Navbar /> */}
                 <div className="row">
 
                 </div>
